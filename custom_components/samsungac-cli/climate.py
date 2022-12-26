43ef76b1c4a7e54c29581f810e6f7ff0b13ddb4c
a1studmuffin <a1studmuffin@gmail.com>
2022-12-26 16:28:59 +1100

    Formatted climate.py, put smartthings bin in more sensible default location

--- custom_components/samsungac-cli/climate.py
+++ custom_components/samsungac-cli/climate.py
@@ -10,7 +10,7 @@ from homeassistant.components.climate.const import (
     SUPPORT_FAN_MODE,
     ATTR_HVAC_MODE,
     ATTR_FAN_MODE,
-    HVAC_MODE_AUTO, 
+    HVAC_MODE_AUTO,
     HVAC_MODE_COOL,
     HVAC_MODE_DRY,
     HVAC_MODE_FAN_ONLY,
@@ -33,6 +33,7 @@ from homeassistant.const import (
     CONF_NAME,
     CONF_ACCESS_TOKEN,
     CONF_DEVICE_ID,
+    TEMP_CELSIUS,
 )
 from homeassistant.helpers.typing import HomeAssistantType, ConfigType
 
@@ -41,16 +42,19 @@ _LOGGER = logging.getLogger(__name__)
 CONF_SMARTTHINGS_PATH = "smartthings_path"
 
 DEFAULT_NAME = "SamsungAC SmartThings CLI"
-DEFAULT_SMARTTHINGS_PATH = "/root/smartthings/smartthings"
+DEFAULT_SMARTTHINGS_PATH = "/usr/local/bin/smartthings"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         voluptuous.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
         voluptuous.Required(CONF_ACCESS_TOKEN): cv.string,
         voluptuous.Required(CONF_DEVICE_ID): cv.string,
-        voluptuous.Required(CONF_SMARTTHINGS_PATH, default=DEFAULT_SMARTTHINGS_PATH): cv.string,
+        voluptuous.Required(
+            CONF_SMARTTHINGS_PATH, default=DEFAULT_SMARTTHINGS_PATH
+        ): cv.string,
     }
 )
+
 
 async def async_setup_platform(
     hass: HomeAssistantType, config: ConfigType, async_add_entities, discovery_info=None
@@ -66,8 +70,8 @@ async def async_setup_platform(
         ]
     )
 
-class SamsungACCLIClimate(ClimateEntity):
 
+class SamsungACCLIClimate(ClimateEntity):
     def __init__(
         self, name: str, access_token: str, device_id: str, smartthings_path: str
     ) -> None:
@@ -93,7 +97,7 @@ class SamsungACCLIClimate(ClimateEntity):
     @property
     def available(self) -> bool:
         return self._available
-        
+
     @property
     def supported_features(self) -> int:
         return SUPPORT_TARGET_TEMPERATURE | SUPPORT_FAN_MODE
@@ -108,7 +112,14 @@ class SamsungACCLIClimate(ClimateEntity):
 
     @property
     def hvac_modes(self):
-        return [HVAC_MODE_AUTO, HVAC_MODE_COOL, HVAC_MODE_DRY, HVAC_MODE_FAN_ONLY, HVAC_MODE_HEAT, HVAC_MODE_OFF]
+        return [
+            HVAC_MODE_AUTO,
+            HVAC_MODE_COOL,
+            HVAC_MODE_DRY,
+            HVAC_MODE_FAN_ONLY,
+            HVAC_MODE_HEAT,
+            HVAC_MODE_OFF,
+        ]
 
     @property
     def min_temp(self):
@@ -117,6 +128,10 @@ class SamsungACCLIClimate(ClimateEntity):
     @property
     def max_temp(self):
         return self._max_temp
+
+    @property
+    def temperature_unit(self):
+        return TEMP_CELSIUS
 
     @property
     def current_temperature(self):
@@ -136,67 +151,99 @@ class SamsungACCLIClimate(ClimateEntity):
 
     def run_smartthings_command(self, command):
         try:
-            smartthings_command = f'{self._smartthings_path} {command} {self._device_id} -j --token {self._access_token}'
+            smartthings_command = f"{self._smartthings_path} {command} {self._device_id} -j --token {self._access_token}"
             _LOGGER.debug(smartthings_command)
-            result = subprocess.run(smartthings_command.split(), capture_output=True, text=True)
+            result = subprocess.run(
+                smartthings_command.split(), capture_output=True, text=True
+            )
             _LOGGER.debug(result.stdout)
             return json.loads(result.stdout)
         except Exception as e:
-            _LOGGER.debug(f'run_smartthings_command() failed: {str(e)}')
-            return ''
+            _LOGGER.debug(f"run_smartthings_command() failed: {str(e)}")
+            return ""
 
     async def async_update(self):
 
-        device_status_json = self.run_smartthings_command('devices:status')
+        device_status_json = self.run_smartthings_command("devices:status")
         try:
             self._available = True
 
-            self._min_temp = device_status_json["components"]["main"]["custom.thermostatSetpointControl"]["minimumSetpoint"]["value"] # just assume celcius here
-            self._max_temp = device_status_json["components"]["main"]["custom.thermostatSetpointControl"]["maximumSetpoint"]["value"] # just assume celcius here
-            
-            self._current_mode = device_status_json["components"]["main"]["airConditionerMode"]["airConditionerMode"]["value"]
-            self._switch_state = device_status_json["components"]["main"]["switch"]["switch"]["value"]
-            if self._switch_state == 'off':
+            self._min_temp = device_status_json["components"]["main"][
+                "custom.thermostatSetpointControl"
+            ]["minimumSetpoint"][
+                "value"
+            ]  # just assume celcius here
+            self._max_temp = device_status_json["components"]["main"][
+                "custom.thermostatSetpointControl"
+            ]["maximumSetpoint"][
+                "value"
+            ]  # just assume celcius here
+
+            self._current_mode = device_status_json["components"]["main"][
+                "airConditionerMode"
+            ]["airConditionerMode"]["value"]
+            self._switch_state = device_status_json["components"]["main"]["switch"][
+                "switch"
+            ]["value"]
+            if self._switch_state == "off":
                 self._action = CURRENT_HVAC_OFF
                 self._mode = HVAC_MODE_OFF
-            elif self._current_mode == 'auto':
+            elif self._current_mode == "auto":
                 self._action = CURRENT_HVAC_IDLE
                 self._mode = HVAC_MODE_AUTO
-            elif self._current_mode == 'cool':
+            elif self._current_mode == "cool":
                 self._action = CURRENT_HVAC_COOL
                 self._mode = HVAC_MODE_COOL
-            elif self._current_mode == 'dry':
+            elif self._current_mode == "dry":
                 self._action = CURRENT_HVAC_DRY
                 self._mode = HVAC_MODE_DRY
-            elif self._current_mode == 'wind':
+            elif self._current_mode == "wind":
                 self._action = CURRENT_HVAC_FAN
                 self._mode = HVAC_MODE_FAN_ONLY
             else:
                 self._action = CURRENT_HVAC_OFF
                 self._mode = HVAC_MODE_OFF
-                _LOGGER.debug(f'Unknown HVAC current mode: {str(self._current_mode)}')
+                _LOGGER.debug(f"Unknown HVAC current mode: {str(self._current_mode)}")
 
-            self._current_fan_mode = device_status_json["components"]["main"]["airConditionerFanMode"]["mode"]["value"]
-            if self._current_fan_mode == 'auto':
+            self._current_fan_mode = device_status_json["components"]["main"][
+                "airConditionerFanMode"
+            ]["mode"]["value"]
+            if self._current_fan_mode == "auto":
                 self._fan_mode = FAN_AUTO
-            elif self.current_mode == 'low':
+            elif self.current_mode == "low":
                 self._fan_mode = FAN_LOW
-            elif self.current_mode == 'medium':
+            elif self.current_mode == "medium":
                 self._fan_mode = FAN_MEDIUM
-            elif self.current_mode == 'high':
+            elif self.current_mode == "high":
                 self._fan_mode = FAN_HIGH
             else:
                 self._fan_mode = FAN_OFF
-                _LOGGER.debug(f'Unknown fan mode: {str(self._fan_mode)}')
+                _LOGGER.debug(f"Unknown fan mode: {str(self._fan_mode)}")
 
-            self._current_temp = device_status_json["components"]["main"]["temperatureMeasurement"]["temperature"]["value"] # just assume celcius here
-            self._target_temp = device_status_json["components"]["main"]["thermostatCoolingSetpoint"]["coolingSetpoint"]["value"] # just assume celcius here
-            self._power_consumption = device_status_json["components"]["main"]["powerConsumptionReport"]["powerConsumption"]["power"] # just assume celcius here
+            self._current_temp = device_status_json["components"]["main"][
+                "temperatureMeasurement"
+            ]["temperature"][
+                "value"
+            ]  # just assume celcius here
+            self._target_temp = device_status_json["components"]["main"][
+                "thermostatCoolingSetpoint"
+            ]["coolingSetpoint"][
+                "value"
+            ]  # just assume celcius here
+            self._power_consumption = device_status_json["components"]["main"][
+                "powerConsumptionReport"
+            ]["powerConsumption"][
+                "power"
+            ]  # just assume celcius here
 
-            _LOGGER.debug(f'State update complete, Target temp: {self._target_temp}; Current temp: {self._current_temp}; Min temp: {self._min_temp}; Max temp: {self._max_temp}; Current mode: {self._current_mode}; Switch state: {self._switch_state}; Current fan mode: {self._current_fan_mode}; Power consumption: {self._power_consumption};')
+            _LOGGER.debug(
+                f"State update complete, Target temp: {self._target_temp}; Current temp: {self._current_temp}; Min temp: {self._min_temp}; Max temp: {self._max_temp}; Current mode: {self._current_mode}; Switch state: {self._switch_state}; Current fan mode: {self._current_fan_mode}; Power consumption: {self._power_consumption};"
+            )
 
         except Exception as e:
-            _LOGGER.debug(f'State update failed to parse json, setting to unavailable: {str(e)}')
+            _LOGGER.debug(
+                f"State update failed to parse json, setting to unavailable: {str(e)}"
+            )
             self._available = False
 
     async def async_set_temperature(self, **kwargs) -> None:
@@ -205,8 +252,10 @@ class SamsungACCLIClimate(ClimateEntity):
             return
 
         self._target_temp = target_temp
-        if self._switch_state == 'on':
-            self.run_smartthings_command(f'thermostatCoolingSetpoint:setCoolingSetpoint({self._target_temp})')
+        if self._switch_state == "on":
+            self.run_smartthings_command(
+                f"thermostatCoolingSetpoint:setCoolingSetpoint({self._target_temp})"
+            )
 
     async def async_set_hvac_mode(self, **kwargs) -> None:
         hvac_mode = kwargs.get(ATTR_HVAC_MODE)
@@ -214,24 +263,30 @@ class SamsungACCLIClimate(ClimateEntity):
             return
 
         self._current_mode = hvac_mode
-        if self._current_mode == 'fan_only':
-            self._current_mode = 'wind'
+        if self._current_mode == "fan_only":
+            self._current_mode = "wind"
 
         # Update switch state from current hvac mode
         prev_switch_state = self._switch_state
-        self._switch_state = 'off' if self._current_mode == 'off' else 'on'        
+        self._switch_state = "off" if self._current_mode == "off" else "on"
         if prev_switch_state != self._switch_state:
-            self.run_smartthings_command(f'switch:{self._switch_state}()')
-        
-        if self._switch_state == 'on':
+            self.run_smartthings_command(f"switch:{self._switch_state}()")
+
+        if self._switch_state == "on":
 
             # Update AC mode if the unit is switched on on
-            self.run_smartthings_command(f'airConditionerMode:setAirConditionerMode({self._current_mode})')
+            self.run_smartthings_command(
+                f"airConditionerMode:setAirConditionerMode({self._current_mode})"
+            )
 
             # also update properties that may have changed while the unit was off
             if prev_switch_state != self._switch_state:
-                self.run_smartthings_command(f'thermostatCoolingSetpoint:setCoolingSetpoint({self._target_temp})')
-                self.run_smartthings_command(f'airConditionerFanMode:setAirConditionerFanMode({self._current_fan_mode})')
+                self.run_smartthings_command(
+                    f"thermostatCoolingSetpoint:setCoolingSetpoint({self._target_temp})"
+                )
+                self.run_smartthings_command(
+                    f"airConditionerFanMode:setAirConditionerFanMode({self._current_fan_mode})"
+                )
 
     async def async_set_fan_mode(self, **kwargs) -> None:
         fan_mode = kwargs.get(ATTR_FAN_MODE)
@@ -240,7 +295,9 @@ class SamsungACCLIClimate(ClimateEntity):
 
         self._current_fan_mode = fan_mode
 
-        if self._switch_state == 'on':
+        if self._switch_state == "on":
 
             # Update AC fan mode if the unit is switched on on
-            self.run_smartthings_command(f'airConditionerFanMode:setAirConditionerFanMode({self._current_fan_mode})')
+            self.run_smartthings_command(
+                f"airConditionerFanMode:setAirConditionerFanMode({self._current_fan_mode})"
+            )
